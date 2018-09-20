3898eb96509c5f53336631eb69a4281b439bfeca
Felix <virtualcastledev@gmail.com>
2018-09-19 20:51:47 -0400

    Initial Commit, Blank Project

---
+++ BabyTube/BabyTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabyTube
+//
+//  Created by Virtual Castle  on 9/19/18.
+//  Copyright © 2018 Virtual Castle . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
